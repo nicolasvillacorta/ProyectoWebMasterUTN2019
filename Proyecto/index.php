6392d09f0e4b9dde49e2119f69de7776a260a698
nicolasvillacorta <villacortav.nicolas@gmail.com>
2019-03-13 04:23:27 -0300

    Add files via upload

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -5,17 +5,7 @@
 	<meta charset="utf-8">
 	<!-- Hacer responsive -->
 	<meta name="viewport" content="width=device-width, user-scalable=no">
-	<link rel="icon" href="img/logos/LogoGallos.png">
-	<!--Linkeo a Bootstrap-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<!--Linkeo a estilos.css-->
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<!--Linkeos a Google fonts-->
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Srisakdi" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	<!--Linkeo a font awesome (iconos)-->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<?php include 'includes/linkeos.php' ?>
 </head>
 
 <body>
---
+++ Proyecto/nuestras_canchas.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Los Gallos - Parque Avellaneda</title>
+	<link rel="icon" href="img/logos/LogoGallos.png">
+	<meta charset="utf-8">
+	<!-- Hacer responsive -->
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<?php include 'includes/linkeos.php' ?>
+</head>
+
+<body>
+	<?php include 'includes/header&Nav.php' ?>
+	<div class="container cuerpo"><br>
+			
+		<div class="row">
+			<div class="col text-center"><b>Cancha 1 (de 5 descubierta).</b><a href="img/fotoCanchas/cancha1.jpg" data-lightbox="image-1" data-title="My caption"><img class="img-thumbnail" src="img/fotoCanchas/cancha1.jpg"></a></div>
+
+			<div class="col text-center"><b>Cancha 2 (de 5 techada).</b><a href="img/fotoCanchas/cancha2.jpg" data-lightbox="image-1" data-title="My caption"><img class="img-thumbnail" src="img/fotoCanchas/cancha2.jpg"></a></div>
+		</div><br><br>
+		<div class="row">
+			<div class="col text-center"><b>Cancha 3 (de 11).</b><a href="img/fotoCanchas/cancha11.jpg" data-lightbox="image-1" data-title="My caption"><img class="img-thumbnail" src="img/fotoCanchas/cancha11.jpg"></a></div>
+
+			<div class="col text-center"><b>Cancha 4 (de 11).</b><a href="img/fotoCanchas/cancha11-2.jpg" data-lightbox="image-1" data-title="My caption"><img class="img-thumbnail" src="img/fotoCanchas/cancha11-2.jpg"></a></div>
+		</div><br>
+		<p class="text-center">Podes reservar cualquiera de nuestras canchas en nuestra seccion de <a href="reservarCancha.php">Reservar Canchas</a>.</p>
+		
+
+
+	</div>
+<?php include 'includes/footer.php' ?>
+<!--jQuery-->
+<script src="js/jQuery.js"></script>
+<!--Script de lightbox-->
+<script src="js/lightbox.js"></script>
+
+<!--Scripts de Bootstrap-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!--Linkeo a script de lightbox-->
+
+</body>
+</html>
