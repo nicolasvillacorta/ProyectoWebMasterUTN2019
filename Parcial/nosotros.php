c83923ca58dc00bbec8fae425d6a644cf9261e4e
nicolasvillacorta <villacortav.nicolas@gmail.com>
2019-03-14 19:58:37 -0300

    Terminado, no "reservar_canchas".
    Proyecto Completo, sin reserva de canchas, totalmente funcional, solo hay que modificar en "procesar_form.php" los datos de la BD, y por ejemplo en 000webhost, cambiar el '' por un 0 en el INSERT INTO.

---
+++ Parcial/nosotros.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Los Gallos - Parque Avellaneda</title>
+	<link rel="icon" href="img/logos/LogoGallos.png">
+	<meta charset="utf-8">
+	<!-- Hacer responsive -->
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<?php include 'includes/linkeos.php' ?>
+</head>
+
+<body>
+	<?php include 'includes/header&Nav.php' ?>
+
+	<div class="container cuerpo text-center mb-5"> <br><p>Somos un grupo de amigos que amamos el futbol, nos conocemos de chicos y nuestro sueño siempre fue el de poder poner nuestra propia cancha de futbol, empezamos allá por 2015 poniendo nuestra primera canchita de futbol 5, tras un gran esfuerzo y un par de meses pudimos colocar nuestra segunda cancha, techada esta vez.</p>
+	<p>Como amamos el asado dijimos: ¿Por que no poner unas parrillitas? Y asi fue como le agregamos las parrillitas al complejo. <br>
+	Pasados dos años de nuestra apertura, en 2017, nos dimos cuenta que mucha gente nos preguntaba por canchas de 11, asi que terminamos por colocarlas. Es asi como hoy en dia tenemos el complejo que pueden ver.</p>
+	<b>¿Por qué "Los Gallos"?</b>
+	<p>Los gallos fue el nombre que decidimos ponerle a nuestro equipo de futbol alla en 2015, es de ahi que traspolamos el nombre al complejo.</p>
+	<div class="text-center">
+		<img class="fotosNos img-fluid" src="img/fotosGallos/1.png"><br><hr>
+		<img class="fotosNos img-fluid" src="img/fotosGallos/2.jpg"><br><hr>
+		<img class="fotosNos img-fluid" src="img/fotosGallos/3.png"><br><hr>
+	</div>
+	<h3 id="firma"><i>Atte. Garrafa, Gobbi, El chacho, Gacela, Pechardo, Pvperto, El colo, La garza, Manzi, Fede </i></h3>
+
+
+	</div>
+
+	</div>
+	<?php include 'includes/footer.php' ?>
+<!--Scripts de Bootstrap-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
